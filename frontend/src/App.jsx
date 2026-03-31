2ed87410365b16b5d9557f1a2f55752f2288a0a8
its-Aman007 <maahivishwaas@gmail.com>
2026-03-31 17:02:46 +0530

    frontend feature

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import About from "./Pages/About";
+import Cart from "./Pages/Cart";
+import Contact from "./Pages/Contact";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Orders from "./Pages/Orders";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Product from "./Pages/Product";
+import Collection from "./Pages/collection";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
+import Verify from "./Pages/Verify";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/verify" element={<Verify />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
